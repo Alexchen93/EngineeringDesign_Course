beefaa976cd4f8c16d45cc9248f4ac914e731d70
Alexchen93 <alex6563834@gmail.com>
2025-05-25 04:39:21 +0800

    精簡化

--- ProjectV6/ProjectV6.ino
+++ ProjectV6/ProjectV6.ino
@@ -9,15 +9,15 @@
 #include "WebPage.h"
 
 // ====== WiFi & Web App ======
-const char* ssid = "ite422";
-const char* password = "ite12345";
+const char* ssid = "SSID";
+const char* password = "Password";
 
 #define RST_PIN   22
 #define SS_PIN    21
 #define LED_PIN   2
 #define SD_CS     5     // SD 卡 CS
 
-const char* CWA_API = "CWA-FFADC16C-8605-4FEA-B52A-02281BA42218";
+const char* CWA_API = "API";
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 WebServer server(80);
